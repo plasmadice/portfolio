53841fecdd0fea1291a04495c11a7e640947ebf9
plasmadice <herman.white.third@gmail.com>
2023-05-18 13:59:37 -0400

    Fix previous portfolio link

--- lib/info.tsx
+++ lib/info.tsx
@@ -44,7 +44,7 @@ export const projects = [
       "My previous portfolio. Built with Next.js, MaterialUI, and hosted on Vercel.",
     technologies: ["React", "Next.js", "MaterialUI"],
     links: {
-      demo: "port-foe-leo.vercel.app",
+      demo: "https://port-foe-leo.vercel.app/",
       github: "https://github.com/plasmadice/port-foe-leo",
     },
   },
