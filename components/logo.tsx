6005ca3ada9b383b1f2a75dfc5023ee6f7dcddfa
plasmadice <19928793+plasmadice@users.noreply.github.com>
2023-09-26 15:48:45 -0400

    Change logo color to primary

--- components/logo.tsx
+++ components/logo.tsx
@@ -206,7 +206,7 @@ export function Logo() {
         <motion.svg
           key={index}
           id="logo"
-          className="text-[#111010] dark:text-white w-[25px] md:w-auto h-[40px] md:h-[70px]"
+          className="text-primary w-[25px] md:w-auto h-[40px] md:h-[70px]"
           width="50"
           height="70"
           viewBox="0 0 50 70"
